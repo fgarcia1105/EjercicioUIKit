326436daad61d466811e4057f349b5f8890d08d7
Raúl Escalante <raul.escalante@elektra.com.mx>
2022-09-22 15:32:49 -0500

    Add observer to the textFields elements

--- UIKitApp/SignUp/SignUpViewController.swift
+++ UIKitApp/SignUp/SignUpViewController.swift
@@ -31,6 +31,38 @@ final class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextFields()
+    }
+
+    private func configureTextFields(){
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard(_:)))
+        self.view.addGestureRecognizer(gesture)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(changekeyboard(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(changekeyboard(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(changekeyboard(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+
+    @objc func hideKeyboard(_ sender: UITapGestureRecognizer) {
+        lblEMail.resignFirstResponder()
+        lblCName.resignFirstResponder()
+        lblSsap.resignFirstResponder()
+    }
+
+    @objc func changekeyboard(notification: Notification){
+        let keyboardInfo = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue
+        let keyboarDimensions = keyboardInfo?.cgRectValue
+        let keyboardHeight = keyboarDimensions?.height
+
+        switch notification.name.rawValue {
+        case "UIKeyboardWillHideNotification":
+            view.frame.origin.y = 0
+        case "UIKeyboardWillShowNotification":
+            if lblEMail.isFirstResponder { view.frame.origin.y = (-1 * keyboardHeight!) * 0.3 }
+            if lblCName.isFirstResponder { view.frame.origin.y = (-1 * keyboardHeight!) * 0.5 }
+            if lblSsap.isFirstResponder { view.frame.origin.y = (-1 * keyboardHeight!) * 0.7 }
+        default: view.frame.origin.y = 0
+        }
     }
     
     // MARK: - IBActions
